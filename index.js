08c03e080c958d2ba4616ad8612ea6e70d2003b5
mustaqimbd <mustaqimkhanbd@gmail.com>
2023-06-04 23:21:43 +0600

    server set up done

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 5000;
+const cors = require('cors');
+require('dotenv').config();
+
+//Middleware
+app.use(cors())
+app.use(express.json())
+
+app.get('/', (req, res) => {
+    res.send('The server is running')
+})
+
+app.listen(port, () => {
+    console.log(`The server is running on ${port} port`);
+})
